dce404dcec1f65bc7aa84d79a4ee65c0b0b6c9ec
Antonio Troiano <antonio.troiano2304@gmail.com>
2022-12-05 12:04:12 +0100

    New Linked list with implementation for Dictionary and sets

--- Single_Linked_List.c
+++ Single_Linked_List.c
@@ -4,7 +4,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define NODE struct Node *
+typedef struct Node Singly_node_t;
+
 #define NULL_CHECK(item)                           \
     {                                              \
         if (!item)                                 \
@@ -16,14 +17,18 @@
 
 struct Node
 {
+    Singly_node_t *linked_node;
     void *data;
-    NODE linked_node;
 };
 
-NODE Get_Tail(NODE *head)
+Singly_node_t *Get_Tail(Singly_node_t **head)
 {
-    NODE current_node = *head;
-    NODE last_node = NULL;
+    if (!*head)
+    {
+        return NULL;
+    }
+    Singly_node_t *current_node = *head;
+    Singly_node_t *last_node = NULL;
     while (current_node)
     {
         last_node = current_node;
@@ -33,82 +38,82 @@ NODE Get_Tail(NODE *head)
     return last_node;
 }
 
-int Append(NODE *head, void *item)
+int Append(Singly_node_t **head, void *item, size_t struct_type)
 {
-    // save the code pointer in a ephermal NODE
-    NODE current_node = Get_Tail(head);
-    NODE appended_node = (NODE)malloc(sizeof(struct Node));
+    Singly_node_t *current_node = Get_Tail(head);
+    Singly_node_t *appended_node = malloc(struct_type);
     NULL_CHECK(appended_node);
-    // define the appended node
     appended_node->data = item;
     appended_node->linked_node = NULL;
     if (!current_node)
     {
         *head = appended_node;
+        printf("I added an element at the begin :%s\n\n", (char *)item);
+
         return 0;
     }
     // define the link at the appended node
     current_node->linked_node = appended_node;
-    return 0;
+    return 1;
 }
 
-void *Pop_First(NODE *head)
+Singly_node_t *Pop_First(Singly_node_t **head)
 {
     if (!*head)
         return NULL;
 
-    NODE current_node = *head;
+    Singly_node_t *current_node = *head;
 
     *head = (*head)->linked_node;
     current_node->linked_node = NULL;
-    return current_node->data;
+    return current_node;
 }
 
-NODE Pop_Last(NODE *head)
+Singly_node_t *Pop_Last(Singly_node_t **head)
 {
     if (!*head)
         return NULL;
-    NODE previous_node = *head;
-    NODE current_node = *head;
+
+    Singly_node_t *previous_node = *head;
+    Singly_node_t *current_node = *head;
     while (current_node->linked_node)
     {
         previous_node = current_node;
         current_node = current_node->linked_node;
     }
     previous_node->linked_node = NULL;
-    // NODE ephe_data = current_node;
-    // // free(current_node);
-    // current_node = NULL;
+
     return current_node;
 }
 
-void *Remove(NODE *head, int index)
+Singly_node_t *Remove(Singly_node_t **head, int index)
 {
-    NODE current_node = *head;
-    NODE previous_node = *head;
-    for (int i = 0; i <= index - 1; i++)
+    Singly_node_t *current_node = *head;
+    Singly_node_t *previous_node = *head;
+    if (index == 0)
     {
-        if (index == 0)
-        {
-            *head = current_node->linked_node;
-            free(current_node);
-            current_node = NULL;
-            return NULL;
-        }
+        Pop_First(head);
+        return current_node;
+    }
+
+    for (int i = 0; i < index; i++)
+    {
         previous_node = current_node;
         current_node = current_node->linked_node;
+        if (!current_node)
+        {
+            printf("Index out of range");
+            return NULL;
+        }
     }
 
     previous_node->linked_node = current_node->linked_node;
-    free(current_node);
-    void *ephe_data = current_node->data;
-    current_node = NULL;
-    return ephe_data;
+    return current_node;
 }
 
-int LinkedListLenght(NODE *head)
+int LinkedListLenght(Singly_node_t **head)
 {
-    NODE temp = *head;
+    Singly_node_t *temp = *head;
     int lenght = 1;
     while (temp->linked_node)
     {
@@ -118,54 +123,56 @@ int LinkedListLenght(NODE *head)
     return lenght;
 }
 
-NODE Revert(NODE *head)
+Singly_node_t *Revert(Singly_node_t **head)
 {
-    NODE *copy_node = head;
-    NODE new_Linked = NULL;
-    NODE Popped;
+    Singly_node_t **copy_node = head;
+    Singly_node_t *new_Linked = NULL;
+    Singly_node_t *Popped;
     int prova = LinkedListLenght(head);
 
     for (size_t i = 0; i < prova; i++)
     {
-        Append(&new_Linked, (struct list_node *)Pop_Last(head)->data);
+        Append(&new_Linked, (struct list_node *)Pop_Last(head)->data, sizeof(Singly_node_t));
     }
     return new_Linked;
 }
 
-int main()
-{
-    NODE head = NULL;
-    Append(&head, "Primo");
-    Append(&head, "Secondo");
-    Append(&head, "Terzo");
-    Append(&head, "Quarto");
-    Append(&head, "Quinto");
-    Append(&head, "Sesto");
-    Append(&head, "Last append");
+#pragma region Main
+// int main()
+// {
+//     Singly_node_t * head = NULL;
+//     Append(&head, "Primo");
+//     Append(&head, "Secondo");
+//     Append(&head, "Terzo");
+//     Append(&head, "Quarto");
+//     Append(&head, "Quinto");
+//     Append(&head, "Sesto");
+//     Append(&head, "Last append");
 
-    printf("%s\n", (char *)(head)->data);
-    printf("Get Tail: %s\n", (char *)Get_Tail(&head)->data);
-    printf("%s\n", (char *)Pop_First(&head));
-    printf("%s\n", (char *)(head)->data);
-    printf("Remove: %s\n", (char *)Remove(&head, 1));
-    printf("%s\n", (char *)(head)->linked_node->data);
-    printf("%s\n", (char *)(((struct list_node *)Pop_Last(&head)->data)));
-    printf("%s\n", (char *)(((struct list_node *)Pop_Last(&head)->data)));
+//     printf("%s\n", (char *)(head)->data);
+//     printf("Get Tail: %s\n", (char *)Get_Tail(&head)->data);
+//     printf("%s\n", (char *)Pop_First(&head));
+//     printf("%s\n", (char *)(head)->data);
+//     printf("Remove: %s\n", (char *)Remove(&head, 0));
+//     printf("%s\n", (char *)(head)->linked_node->data);
+//     printf("%s\n", (char *)(((struct list_node *)Pop_Last(&head)->data)));
+//     printf("%s\n", (char *)(((struct list_node *)Pop_Last(&head)->data)));
 
-    NODE prova = head;
-    while (prova)
-    {
-        printf("%s\n", (char *)prova->data);
-        prova = prova->linked_node;
-    }
+//     Singly_node_t * prova = head;
+//     while (prova)
+//     {
+//         printf("%s\n", (char *)prova->data);
+//         prova = prova->linked_node;
+//     }
 
-    NODE reverted = Revert(&head);
-    NODE Revert_head = reverted;
-    while (Revert_head)
-    {
-        printf("%s\n", (char *)Revert_head->data);
-        Revert_head = Revert_head->linked_node;
-    }
+//     Singly_node_t * reverted = Revert(&head);
+//     Singly_node_t * Revert_head = reverted;
+//     while (Revert_head)
+//     {
+//         printf("%s\n", (char *)Revert_head->data);
+//         Revert_head = Revert_head->linked_node;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+#pragma endregion
